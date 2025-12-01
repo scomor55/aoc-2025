a78e124c50c53c500c3dca57c3ea043e4e39e431
scomor55 <scomor2@etf.unsa.ba>
2025-12-01 22:35:30 +0100

    day1

---
+++ Day1/Day1Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+var input = File.ReadAllLines("input.txt");
+var instructions = input.Select(s => (direction: s[0], steps: int.Parse(s[1..]))).ToList();
+int dial = 50;
+int COUNTER = 0;
+foreach (var (direction, steps) in instructions)
+{
+  //  Console.WriteLine($"{direction} {steps}");
+    if(direction == 'R')
+    {
+        dial = (dial+steps) % 100;
+    }
+    else if(direction == 'L')
+    {
+        dial = (dial - steps%100 +100) % 100;
+    }
+    if (dial == 0) COUNTER++;
+}
+
+Console.WriteLine($"Password {COUNTER}");
---
+++ Day1/Day1Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+var input = File.ReadAllLines("input.txt");
+var instructions = input.Select(s => (direction: s[0], steps: int.Parse(s[1..]))).ToList();
+int dial = 50;
+int COUNTER = 0;
+foreach (var (direction, steps) in instructions)
+{
+    for (int i = 0; i < steps; i++)
+    {
+        //  Console.WriteLine($"{direction} {steps}");
+        if (direction == 'R')
+        {
+            dial = (dial + 1) % 100;
+        }
+        else if (direction == 'L')
+        {
+            dial = (dial - 1 + 100) % 100;
+        }
+        if (dial == 0) COUNTER++;
+    }
+}
+
+Console.WriteLine($"Password {COUNTER}");
